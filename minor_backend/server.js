74c873ec08a1d8a91b7c6e08241a3b634dd9bb19
RISHI JAIN <rishijain@RISHIs-MacBook-Air.local>
2025-05-02 14:29:18 +0530

    adding frontend LINK

--- minor_backend/server.js
+++ minor_backend/server.js
@@ -14,7 +14,6 @@ const PORT = process.env.PORT || 7654;
 // Middleware setup
 app.use(cors({
   origin: [
-    "https://book-share-frontend-beta.vercel.app",
     "https://book-share-frontend-2yntf61qh-rishijainns-projects.vercel.app"
   ],
   credentials: true
